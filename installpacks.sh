cf6b4ca13065f14b6278fed10777a94b6b30d6db
TetsuroAndo <tetsuro997@gmail.com>
2024-10-28 17:17:28 +0900

    fix

--- installpacks.sh
+++ installpacks.sh
@@ -1,6 +1,6 @@
 su
 apt update && apt upgrade -y && \
-apt install -y sudo wget git vim openssh-server ufw libpam-pwquality lvm2 cron net-tools
+apt install -y sudo wget curl git vim openssh-server ufw libpam-pwquality lvm2 cron net-tools
 
 # bonus
 apt install -y lighttpd mariadb-server php php-mysql php-cgi
